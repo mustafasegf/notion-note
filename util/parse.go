73bae6717f087a7be6205bec4e2a2226978d618e
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2021-08-10 20:51:00 +0700

    feat: enable new line

--- util/parse.go
+++ util/parse.go
@@ -14,15 +14,12 @@ func CheckIfCommand(body string) (valid bool) {
 func GetCommand(body string) (command string) {
 	lines := strings.SplitN(body, " ", 2)
 	command = lines[0][1:]
-
 	return
 }
 
 func ParseText(body string) (title, content string) {
-	if body[0:6] == "/note " {
-		lines := strings.SplitN(body, "\n", 2)
-		title = lines[0][6:]
-		content = strings.ReplaceAll(lines[1], "\n", "")
-	}
+	lines := strings.SplitN(body[6:], "\n", 2)
+	title = lines[0]
+	content = lines[1]
 	return
 }
